e9590963277cd9f04aeb1397a4e4aa64cf643da7
yeziyqf <511198980@qq.com>
2017-04-26 19:47:04 +0800

    Adding multi threads time specification. Print to be done.

--- sim-cosine_yiming/sim-cosine/source/main.cpp
+++ sim-cosine_yiming/sim-cosine/source/main.cpp
@@ -20,16 +20,17 @@
 #include <sys/timeb.h>
 #include <map>
 #include <typeinfo>
+#include <sys/types.h>
 
 
 #include "similarity.hpp" /// create headers
 
 using namespace std;
 
-namespace { 
-  const size_t SUCCESS = 0; 
-  const size_t ERROR_IN_COMMAND_LINE = 1; 
-  const size_t ERROR_UNHANDLED_EXCEPTION = 2;  
+namespace {
+	const size_t SUCCESS = 0;
+	const size_t ERROR_IN_COMMAND_LINE = 1;
+	const size_t ERROR_UNHANDLED_EXCEPTION = 2;
 }
 #define _LINE_LENGTH 300
 
@@ -47,7 +48,7 @@ size_t mem=0;
 //    size_t l_mem=0;
 int pid=0;
 int tid=-1;
-long long start,end1,end_sim,end_push,end_sort,end_itend;
+long long start,end1,end_sim,end_push,end_sort,end_itend,begin_sort1,end_sort1;
 multimap<size_t,itemS> neighbors1,neighbors2,neighbors3,
 		neighbors4,neighbors5,neighbors6;
 vector<PAIR> neighbors_vec1,neighbors_vec2,neighbors_vec3,
@@ -57,32 +58,32 @@ vector<PAIR> neighbors_vec1,neighbors_vec2,neighbors_vec3,
   Parse command line parameters
 */
 size_t parse_params(int argc, char *argv[], string& ratings_path, unsigned
- int& num_users, unsigned int& num_items, unsigned int& nearest_neighbors,
- float& asymmetric_alpha, float& supp_threshold, float& conf_threshold, unsigned
- int& locality_param, string& targets_path) {
+int& num_users, unsigned int& num_items, unsigned int& nearest_neighbors,
+					float& asymmetric_alpha, float& supp_threshold, float& conf_threshold, unsigned
+					int& locality_param, string& targets_path) {
 	namespace po = boost::program_options;
 	po::options_description desc("Program parameters");
 	desc.add_options()
-		("help,?", "produce help message")
-		("num_users,u", po::value<unsigned int>(&num_users)->required(),
+			("help,?", "produce help message")
+			("num_users,u", po::value<unsigned int>(&num_users)->required(),
 			"number of users")
-		("num_items,i", po::value<unsigned int>(&num_items)->required(),
+	("num_items,i", po::value<unsigned int>(&num_items)->required(),
 			"number of items")
-		("ratings_matrix,r", po::value<string>(&ratings_path)->required(),
+	("ratings_matrix,r", po::value<string>(&ratings_path)->required(),
 			"ratings matrix path")
-		("target_users,t", po::value<string>(&targets_path)->required(),
-			"target users ratings matrix path")
-		("nearest_neighbors,k",
-			po::value<unsigned int>(&nearest_neighbors)->default_value(0),
+			("target_users,t", po::value<string>(&targets_path)->required(),
+			 "target users ratings matrix path")
+			("nearest_neighbors,k",
+			 po::value<unsigned int>(&nearest_neighbors)->default_value(0),
 			"output exclusively the k nearest neighbors for each user")
-		("asymmetric_alpha,a", po::value<float>(&asymmetric_alpha)->default_value(0.5),
+	("asymmetric_alpha,a", po::value<float>(&asymmetric_alpha)->default_value(0.5),
 			"asymmetric cosine parameter -- default: symmetric (0.5)")
-		("supp_threshold,s", po::value<float>(&supp_threshold)->default_value(UNDEFTH),
-			"support threshold")
-		("conf_threshold,c", po::value<float>(&conf_threshold)->default_value(UNDEFTH),
-			"confiance threshold")
-		("locality_param,q",
-			po::value<unsigned int>(&locality_param)->default_value(1),
+			("supp_threshold,s", po::value<float>(&supp_threshold)->default_value(UNDEFTH),
+			 "support threshold")
+			("conf_threshold,c", po::value<float>(&conf_threshold)->default_value(UNDEFTH),
+			 "confiance threshold")
+			("locality_param,q",
+			 po::value<unsigned int>(&locality_param)->default_value(1),
 			"transforms the similarity as follows: x->x^q")
 	;
 	po::variables_map vm;
@@ -209,82 +210,82 @@ bool RankbyScore(const PAIR left, const PAIR right) {
 
 size_t get_executable_path( char* processdir,char* processname, size_t len)
 {
-    char* path_end;
-    if(readlink("/proc/self/exe", processdir,len) <=0)
-        return -1;
-    path_end = strrchr(processdir,  '/');
-    if(path_end == NULL)
-        return -1;
-    ++path_end;
-    strcpy(processname, path_end);
-    *path_end = '\0';
-    return (size_t)(path_end - processdir);
+	char* path_end;
+	if(readlink("/proc/self/exe", processdir,len) <=0)
+		return -1;
+	path_end = strrchr(processdir,  '/');
+	if(path_end == NULL)
+		return -1;
+	++path_end;
+	strcpy(processname, path_end);
+	*path_end = '\0';
+	return (size_t)(path_end - processdir);
 }
 
 
 bool GetCpuMem(float &cpu,size_t &mem, int pid,int tid = -1)
 {
-    bool ret = false;
-    char cmdline[100];
-    sprintf(cmdline, "ps -o %%cpu,rss,%%mem,pid,tid -mp %d", pid);
-    FILE *file;
-    file = popen(cmdline, "r");
-    if (file == NULL)
-    {
-        printf("file == NULL\n");
-        return false;
-    }
+	bool ret = false;
+	char cmdline[100];
+	sprintf(cmdline, "ps -o %%cpu,rss,%%mem,pid,tid -mp %d", pid);
+	FILE *file;
+	file = popen(cmdline, "r");
+	if (file == NULL)
+	{
+		printf("file == NULL\n");
+		return false;
+	}
 
-    char line[_LINE_LENGTH];
-    float l_cpuPrec=0;
-    int l_mem=0;
-    float l_memPrec=0;
-    int l_pid=0;
-    int l_tid=0;
-    if (fgets(line, _LINE_LENGTH, file) != NULL)
-    {
-        //  printf("1st line:%s",line);
-        if (fgets(line, _LINE_LENGTH, file) != NULL)
-        {
-            //      printf("2nd line:%s",line);
-            sscanf( line, "%f %d %f %d -", &l_cpuPrec, &l_mem, &l_memPrec, &l_pid );
-            cpu = l_cpuPrec;
+	char line[_LINE_LENGTH];
+	float l_cpuPrec=0;
+	int l_mem=0;
+	float l_memPrec=0;
+	int l_pid=0;
+	int l_tid=0;
+	if (fgets(line, _LINE_LENGTH, file) != NULL)
+	{
+		//  printf("1st line:%s",line);
+		if (fgets(line, _LINE_LENGTH, file) != NULL)
+		{
+			//      printf("2nd line:%s",line);
+			sscanf( line, "%f %d %f %d -", &l_cpuPrec, &l_mem, &l_memPrec, &l_pid );
+			cpu = l_cpuPrec;
 //            mem = l_mem/1024;
-            mem = l_mem /1;
-            if( tid == -1 )
-                ret = true;
-            else
-            {
-                while( fgets(line, _LINE_LENGTH, file) != NULL )
-                {
-                    sscanf( line, "%f - - - %d", &l_cpuPrec, &l_tid );
-                    //              printf("other line:%s",line);
-                    //              cout<<l_cpuPrec<<'\t'<<l_tid<<endl;
-                    if( l_tid == tid )
-                    {
-                        printf("cpuVal is tid:%d\n",tid);
-                        cpu = l_cpuPrec;
-                        ret = true;
-                        break;
-                    }
-                }
-                if( l_tid != tid )
-                    printf("TID not exist\n");
-            }
-        }
-        else
-            printf("PID not exist\n");
-    }
-    else
-        printf("Command or Parameter wrong\n");
-    pclose(file);
-    return ret;
+			mem = l_mem /1;
+			if( tid == -1 )
+				ret = true;
+			else
+			{
+				while( fgets(line, _LINE_LENGTH, file) != NULL )
+				{
+					sscanf( line, "%f - - - %d", &l_cpuPrec, &l_tid );
+					//              printf("other line:%s",line);
+					//              cout<<l_cpuPrec<<'\t'<<l_tid<<endl;
+					if( l_tid == tid )
+					{
+						printf("cpuVal is tid:%d\n",tid);
+						cpu = l_cpuPrec;
+						ret = true;
+						break;
+					}
+				}
+				if( l_tid != tid )
+					printf("TID not exist\n");
+			}
+		}
+		else
+			printf("PID not exist\n");
+	}
+	else
+		printf("Command or Parameter wrong\n");
+	pclose(file);
+	return ret;
 }
 
 long long getSystemTime() {
-    struct timeb t;
-    ftime(&t);
-    return 1000 * t.time + t.millitm;
+	struct timeb t;
+	ftime(&t);
+	return 1000 * t.time + t.millitm;
 }
 
 void compute_similarity(int division, int group,
@@ -293,38 +294,38 @@ void compute_similarity(int division, int group,
 	cerr << "Computing Similarity......." << endl;
 
 //	for (ptrdiff_t row=0; ((long(row % division) == long(group))&&(row < targets.outerSize())); ++row) {
-    for (ptrdiff_t row=0; (row < targets.outerSize()); ++row) {
+	for (ptrdiff_t row=0; (row < targets.outerSize()); ++row) {
 //		cerr << " " << row << " ";
 //        cerr << "group is: " << group << ", division is: " << division << ", row % division is: " << row % division << endl;
 //        cerr << "type of row % division: " << typeid(row % division).name() << ", type of group: " << typeid(long(group)).name() << endl;
-        if (long(row % division) == long(group)){
-            for (size_t k=0; k < ratings.outerSize(); ++k) {
+		if (long(row % division) == long(group)){
+			for (size_t k=0; k < ratings.outerSize(); ++k) {
 //			 cerr << row << ' ' << k << ' ' << targets.innerVector(row).nonZeros()
 //			  << ' ' <<  ratings.innerVector(k).nonZeros() << ' ' << sim << '\n';
-                if (targets.innerVector(row).nonZeros() &&
-                    ratings.innerVector(k).nonZeros()) {
-//				if ((row == 10)&&(k == 10)) {
-//					start = getSystemTime();
-//				}
+				if (targets.innerVector(row).nonZeros() &&
+					ratings.innerVector(k).nonZeros()) {
+					if ((row == 10)&&(k == 10)) {
+						start = getSystemTime();
+					}
 
-                    innerVec tar = targets.innerVector(row), rat = ratings.innerVector(k);
-//				if ((row == 10)&&(k == 10)) {
-//					GetCpuMem(cpu, mem, pid, tid);
-//					cerr << "Compute tar and rat: " << "CPU: " << cpu << " " << "MEM: " << mem;
-//					end1 = getSystemTime();
-//					cerr << " Time for computing tar and rat: " << end1 - start << "ms" << endl;
-//				}
+					innerVec tar = targets.innerVector(row), rat = ratings.innerVector(k);
+					if ((row == 10)&&(k == 10)) {
+						GetCpuMem(cpu, mem, pid, tid);
+						cerr << "Compute tar and rat: " << "CPU: " << cpu << " " << "MEM: " << mem;
+						end1 = getSystemTime();
+						cerr << " Time for computing tar and rat: " << end1 - start << "ms" << endl;
+					}
 
-                    rating_t sim = cosine_sim(tar,rat,
-                                              asymmetric_alpha, locality_param, supp_threshold);
+					rating_t sim = cosine_sim(tar,rat,
+											  asymmetric_alpha, locality_param, supp_threshold);
 //				cerr << "The " << row << "st round, Similarity is " << sim << endl;
-//				if ((row == 10)&&(k == 10)) {
-//					GetCpuMem(cpu, mem, pid, tid);
-//					cerr << "Similarities: " << "CPU: " << cpu << " " << "MEM: " << mem;
-//					end_sim = getSystemTime();
-//					cerr << " Time for computing Similarities: " << end_sim - end1 << "ms" << endl;
-//				}
-                    if (sim > 0.0) {
+					if ((row == 10)&&(k == 10)) {
+						GetCpuMem(cpu, mem, pid, tid);
+						cerr << "Similarities: " << "CPU: " << cpu << " " << "MEM: " << mem;
+						end_sim = getSystemTime();
+						cerr << " Time for computing Similarities: " << end_sim - end1 << "ms" << endl;
+					}
+					if (sim > 0.0) {
 //					pair<size_t,size_t> newpair(k,sim);
 
 //					std::pair<size_t, rating_t> newpair;
@@ -332,7 +333,7 @@ void compute_similarity(int division, int group,
 //					neighbors.insert(row,newpair);
 
 //					itemS newpair(k,sim);
-                        itemS newpair = {k,sim};
+						itemS newpair = {k,sim};
 						if (group == 1){
 							neighbors1.insert(make_pair(row,newpair));
 						}
@@ -353,29 +354,35 @@ void compute_similarity(int division, int group,
 						}
 
 //                        neighbors.insert(make_pair(row,newpair));
-                    }
-//				if ((row == 10)&&(k == 10)) {
-//					GetCpuMem(cpu, mem, pid, tid);
-//					cerr << "For pushing back: " << "CPU: " << cpu << " " << "MEM: " << mem;
-//					end_push = getSystemTime();
-//					cerr << " Time for computing pushing back: " << end_push - end_sim << "ms" << endl;
-//				}
-                }
-                // cout << row << ' ' << k << ' ' << targets.innerVector(row).nonZeros()
-                //  << ' ' <<  ratings.innerVector(k).nonZeros() << ' ' << sim << '\n';
-                // cout << targets.innerVector(row) << ratings.innerVector(k) << "\n\n";
-            }
+					}
+					if ((row == 10)&&(k == 10)) {
+						GetCpuMem(cpu, mem, pid, tid);
+						cerr << "For pushing back(insert): " << "CPU: " << cpu << " " << "MEM: " << mem;
+						end_push = getSystemTime();
+						cerr << " Time for pushing back(insert): " << end_push - end_sim << "ms" << endl;
+					}
+				}
+				// cout << row << ' ' << k << ' ' << targets.innerVector(row).nonZeros()
+				//  << ' ' <<  ratings.innerVector(k).nonZeros() << ' ' << sim << '\n';
+				// cout << targets.innerVector(row) << ratings.innerVector(k) << "\n\n";
+			}
 
 //			cerr << "Sort done." << endl;
-        }
+		}
 
 //		neighbors.clear(); //for single threads.
 	}
 
 	if (group == 1){
+		begin_sort1 = getSystemTime();
 		vector<PAIR> neighbors_vec1(neighbors1.begin(), neighbors1.end());
 //				cerr << "Vector copy finishes." << endl;
 		sort(neighbors_vec1.begin(),neighbors_vec1.end(),RankbyScore);
+
+		GetCpuMem(cpu, mem, pid, tid);
+		cerr << "Sorting for group 1: " << "CPU: " << cpu << " " << "MEM: " << mem;
+		end_sort1 = getSystemTime();
+		cerr << " Time for sorting group 1: " << end_sort1 - begin_sort1 << "ms" << endl;
 	}
 	if (group == 2){
 		vector<PAIR> neighbors_vec2(neighbors2.begin(), neighbors2.end());
@@ -396,6 +403,9 @@ void compute_similarity(int division, int group,
 		vector<PAIR> neighbors_vec5(neighbors5.begin(), neighbors5.end());
 //				cerr << "Vector copy finishes." << endl;
 		sort(neighbors_vec5.begin(),neighbors_vec5.end(),RankbyScore);
+
+		GetCpuMem(cpu, mem, pid, tid);
+		cerr << "Sorting for group 5: " << "CPU: " << cpu << " " << "MEM: " << mem;
 	}
 	if (group == 6){
 		vector<PAIR> neighbors_vec6(neighbors6.begin(), neighbors6.end());
@@ -412,22 +422,22 @@ void compute_similarity(int division, int group,
 void printResult(unsigned int nearest_neighbors){
 	//print the result.
 	cerr << "print result procedure." << endl;
-    multimap<size_t,itemS>::iterator iter,iter1,iter2;
+	multimap<size_t,itemS>::iterator iter,iter1,iter2;
 //	vector<PAIR> neighbors_vec(neighbors.begin(), neighbors.end());
 //	cerr << "Vector copy finishes." << endl;
 //	sort(neighbors_vec.begin(),neighbors_vec.end(),RankbyScore);
 //	cerr << "Sort done." << endl;
 
-	
-//	GetCpuMem(cpu, mem, pid, tid);
-//	cerr << "For Sorting: " << "CPU: " << cpu << " " << "MEM: " << mem;
-//	end_sort = getSystemTime();
-//	cerr << " Time for Sorting afterwards: " << end_sort - end_push << "ms" << endl;
+
+	GetCpuMem(cpu, mem, pid, tid);
+	cerr << "For Sorting: " << "CPU: " << cpu << " " << "MEM: " << mem;
+	end_sort = getSystemTime();
+	cerr << " Time for Sorting afterwards: " << end_sort - end_push << "ms" << endl;
 
 //	auto itend = (nearest_neighbors && nearest_neighbors < neighbors.size())?
 //				 neighbors.begin()+nearest_neighbors : neighbors.end();
 	int neighbors_size_total = neighbors1.size() + neighbors2.size() + neighbors3.size()
-								+ neighbors4.size() + neighbors5.size() + neighbors6.size();
+							   + neighbors4.size() + neighbors5.size() + neighbors6.size();
 	int itend_int = (nearest_neighbors && nearest_neighbors < neighbors_size_total)?
 					nearest_neighbors : neighbors_size_total;
 	cerr << "nearest neighbour is:" << nearest_neighbors << endl;
@@ -455,12 +465,13 @@ void printResult(unsigned int nearest_neighbors){
 //	}
 	itend_int = 100;
 	//Multimap_vector output.
-	for (int i = 0; i != itend_int; ++i) {
+	for (int i = 1; i != itend_int; ++i) {
 		cerr << "hello " << i;
-		if (neighbors_vec1[i].first != neighbors_vec1[i].second.otherItem){
-			cerr << neighbors_vec1[i].first << ' ' << neighbors_vec1[i].second.otherItem
-				 << ' ' << neighbors_vec1[i].second.score << endl;
-		}
+//		if (neighbors_vec1[i].first != neighbors_vec1[i].second.otherItem){
+		cerr << "Not the same comparison.";
+		cerr << neighbors_vec1[i].first << ' neighbors_vec1[i].first finished ' << neighbors_vec1[i].second.otherItem
+			 << ' neighbors_vec1[i].second.otherItem finished ' << neighbors_vec1[i].second.score << endl;
+//		}
 	}
 	cerr << "1";
 	for (int i = 0; i != itend_int; ++i) {
@@ -507,14 +518,13 @@ void printResult(unsigned int nearest_neighbors){
 }
 
 int main (int argc, char *argv[]) {
-
 	int division = 6;
 	int group;
 
 
-    char path[PATH_MAX];
-    char processname[1024];
-    get_executable_path(path, processname, sizeof(path));
+	char path[PATH_MAX];
+	char processname[1024];
+	get_executable_path(path, processname, sizeof(path));
 //    printf("directory:%s\nprocessname:%s\n",path,processname);
 //    cerr << now() << "directory:%s\nprocessname:%s\n",path,processname;
 	auto n_core = thread::hardware_concurrency();//获取cpu核心个数
@@ -524,17 +534,17 @@ int main (int argc, char *argv[]) {
 	unsigned int num_users, num_items, nearest_neighbors, locality_param;
 	float asymmetric_alpha, supp_threshold, conf_threshold;
 	string ratings_path, targets_path;
-    pid = getpid();
-    cerr << now() << " pid is:" << pid << endl;
-    GetCpuMem( cpu, mem, pid, tid );
-    cerr << now() << " Before parsing, " << "CPU: " << cpu << " " << "MEM: " << mem << endl;
+	pid = getpid();
+	cerr << now() << " pid is:" << pid << endl;
+	GetCpuMem( cpu, mem, pid, tid );
+	cerr << now() << " Before parsing, " << "CPU: " << cpu << " " << "MEM: " << mem << endl;
 
 
 
 
-    if (parse_params(argc, argv, ratings_path, num_users, num_items,
-		nearest_neighbors, asymmetric_alpha, supp_threshold, conf_threshold,
-		locality_param,targets_path)) {
+	if (parse_params(argc, argv, ratings_path, num_users, num_items,
+					 nearest_neighbors, asymmetric_alpha, supp_threshold, conf_threshold,
+					 locality_param,targets_path)) {
 		return ERROR_IN_COMMAND_LINE;
 	}
 
@@ -550,16 +560,16 @@ int main (int argc, char *argv[]) {
 		entries = importTriplets<rating_t, Triplet<rating_t> >(triplets, ratings_path);
 		ratings.setFromTriplets(triplets.begin(), triplets.end());
 		cerr << entries << " entries loaded." << endl;
-        GetCpuMem( cpu, mem, pid, tid );
-        cerr << now() << " After loading entries, " << "CPU: " << cpu << " " << "MEM: " << mem << endl;
+		GetCpuMem( cpu, mem, pid, tid );
+		cerr << now() << " After loading entries, " << "CPU: " << cpu << " " << "MEM: " << mem << endl;
 
 		triplets.clear();
 		cerr << now() << " Loading target user rating matrix... ";
 		entries = importTriplets<rating_t, Triplet<rating_t> >(triplets, targets_path);
 		targets.setFromTriplets(triplets.begin(), triplets.end());
 		cerr << entries << " entries loaded." << endl;
-        GetCpuMem( cpu, mem, pid, tid );
-        cerr << now() << " After loading target user rating matrix, " << "CPU: " << cpu << " " << "MEM: " << mem << endl;
+		GetCpuMem( cpu, mem, pid, tid );
+		cerr << now() << " After loading target user rating matrix, " << "CPU: " << cpu << " " << "MEM: " << mem << endl;
 	} catch (string errmsg) {
 		cerr << "Error: " << errmsg << endl;
 		terminate();
@@ -570,7 +580,7 @@ int main (int argc, char *argv[]) {
 //    MEM_OCCUPY mem_stat;
 //    int cpu;
 
-    //Print the mem used now.
+	//Print the mem used now.
 //    cerr << now() << " Memory used now is:" << get_memoccupy ((MEM_OCCUPY *)&mem_stat) << endl;
 
 
@@ -584,8 +594,8 @@ int main (int argc, char *argv[]) {
 	cerr << "Rating outsize is: " << ratings.outerSize() << endl;
 
 	std::thread t1(compute_similarity, division, 1,
-			ratings, targets, locality_param,
-			  asymmetric_alpha, supp_threshold, nearest_neighbors);
+				   ratings, targets, locality_param,
+				   asymmetric_alpha, supp_threshold, nearest_neighbors);
 	std::thread t2(compute_similarity, division, 2,
 				   ratings, targets, locality_param,
 				   asymmetric_alpha, supp_threshold, nearest_neighbors);
@@ -601,7 +611,7 @@ int main (int argc, char *argv[]) {
 //	std::thread t6(compute_similarity, division, 0, std::ref(neighbors),
 //				   ratings, targets, locality_param,
 //				   asymmetric_alpha, supp_threshold, nearest_neighbors);
-    t1.join();
+	t1.join();
 	t2.join();
 	t3.join();
 	t4.join();
